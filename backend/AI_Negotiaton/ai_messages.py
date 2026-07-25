fea78cea838a7c89669b3702e45f08accedc2d75
Souvik Saha <souviksh2004@gmail.com>
2026-07-26 00:58:29 +0530

    the negotiation logic has been changed 2

--- backend/AI_Negotiaton/ai_messages.py
+++ backend/AI_Negotiaton/ai_messages.py
@@ -8,25 +8,33 @@ model = genai.GenerativeModel("models/gemini-3.1-flash-lite")
 
 def generate_message(decision, price,user_price):
     if decision == "COUNTER":
-        prompt=f"""
-        1.you are a GenZ and a pro negotiator so you have to counter accordinly 
-        2.you have to maximize profits by testing the user with the negotiation but in one precise sentence
-        3.the actual price is {price} rupees 
-        4.the user offered price is {user_price}
-        5.now you have to get a very profitable price for this product by giving the user a bit of room for negotiation 
-    """
+        prompt = f"""
+        You are a friendly shopkeeper.
+
+        The customer's offer is ₹{user_price}.
+
+        The system has already decided the counter offer as ₹{price}.
+
+        Reply in ONE short, natural sentence.
+
+        Mention the counter price exactly as ₹{price}.
+
+        Do not change the amount.
+        Do not calculate another price.
+        Simply persuade the customer to accept ₹{price}.
+        """
     elif decision=="ACCEPT":
         prompt=f"""
-        1.you are a GenZ and a pro negotiator so you have to answer accordinly just like a friend  
-        2.thank the user in a GenZ way for the price but in one precise sentence
+        1.you are a pro negotiator so you have to answer accordinly just like a friend  
+        2.thank the user  for the price but in one precise sentence
         3.just inform the user in the last that final price in a precise chat
         4.the actual price is {price} rupees 
         5.the user offered price is {user_price}
     """
     else:
         prompt=f"""
-        1.you are a GenZ and a pro negotiator so you have to counter accordinly 
-        2.reject the user offer in a GenZ way and give a small bye bye note also but in one precise sentence
+        1.you are a pro negotiator so you have to counter accordinly 
+        2.reject the user offer and give a small bye bye note also but in one precise sentence
         3.the actual price is {price} rupees 
         4.the user offered price is {user_price}
     """
